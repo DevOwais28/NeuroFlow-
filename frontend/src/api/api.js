964b60698983fc2221dc4e1286c72d3f65a47294
Owais Ahmed <153555180+DevOwais28@users.noreply.github.com>
2026-05-09 10:49:27 +0500

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://neuroflow-production-845c.up.railway.app",
   headers: { "Content-Type": "application/json" },
   timeout: 10000,
 });
